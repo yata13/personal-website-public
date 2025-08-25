aa1754858a744c8d0b1d745e6ba48faba5041444
yared tesfahun <122173310+yata13@users.noreply.github.com>
2025-08-26 00:04:19 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const allowed = [
   'http://127.0.0.1:5500',
   'http://localhost:5500',
   'http://localhost:3000',
-  https://yaredpersenalweb.netlify.app/
+  'https://yaredpersenalweb.netlify.app'
 ];
 
 app.use(
@@ -122,4 +122,5 @@ const port = Number(process.env.PORT || 5000);
 app.listen(port, () => {
   console.log(`App listening on port ${port}…`);
 });
+
 
